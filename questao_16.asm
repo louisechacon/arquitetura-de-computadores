307408656bad788c3c5d3c8ad8b555c3567bc59f
Louise Chacon <louisechacon26@gmail.com>
2026-03-16 23:49:00 -0300

    adiciona questão 16

---
+++ questao_16.asm
@@ -0,0 +1,19 @@
+# Faça um programa que leia um número inteiro e imprima -1 se o valor for ímpar e 0 se o valor for par.
+
+.text
+main:	addi $2, $0, 5
+	syscall
+	add $8, $0, $2
+	
+	addi $9, $0, 2
+	
+	div $8, $9
+	mfhi $10
+	sub $10, $0, $10
+	
+	add $4, $0, $10
+	addi $2, $0, 1
+	syscall
+	
+	addi $2, $0, 10
+	syscall
